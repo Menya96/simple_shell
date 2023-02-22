5a4c70feb318ddc11f32078701461d6bdf30a7cd
Menya96 <menyahtonny@gmail.com>
2023-02-23 01:02:08 +0300

    main.c

--- main.c
+++ main.c
@@ -15,17 +15,16 @@ int fd = 2;
 
 asm("mov %1, %0\n\t"
 "add $3, %0"
-:"=r"(fd)
-:"r"(fd));
-        
-if(ac == 2)
+: "=r"(fd)
+: "r"(fd));      
+if (ac == 2)
 {
 fd = open(av[1, O_RDONLY]);
-if(fd == 1)
+if (fd == 1)
 {
-if(errno == EACCES)
+if (errno == EACCES)
 exit(126);
-if(errno ==ENOENT)
+if(errno == ENOENT)
 {
 _eputs(av[0]);
 _eputs(":0: Can't Open ");
