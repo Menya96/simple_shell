083baf8071a6a8c15e72cf38a85089e23ba94624
root <you@example>
2023-02-22 21:58:39 -0800

    fixing errors

--- history.c
+++ history.c
@@ -116,7 +116,7 @@ int build_history_list(info_t *info, char *buf, int linecount)
 	return (0);
 }
 
-/*
+/**
 * renumber_history - renumbers the history linked list after changes
 * @info: Structure containing potential arguments. Used to maintain
 * Return: the new histcount
